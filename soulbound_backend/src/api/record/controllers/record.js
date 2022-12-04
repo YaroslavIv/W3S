201bcb8308f6e5cc909c4c8b2c90ddc6712a9abb
YaroslavIv <yroslavcr@gmail.com>
2022-12-04 16:49:14 +0300

    add docker, reconfigurate db

--- soulbound_backend/src/api/record/controllers/record.js
+++ soulbound_backend/src/api/record/controllers/record.js
@@ -1,9 +1,24 @@
-'use strict';
+"use strict";
 
 /**
  * record controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::record.record');
+module.exports = createCoreController("api::record.record", ({ strapi }) => ({
+  async find(ctx) {
+    const { query } = ctx;
+
+    const entity = await strapi.entityService.findMany("api::record.record", {
+      ...query,
+      populate: {
+        links: true,
+        wallets: true,
+      },
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
